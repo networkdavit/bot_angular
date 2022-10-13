93d371e0c995a90e9ab7c6688b1a6ba6e5c19526
networkdavit <lonecreation1612@gmail.com>
2022-10-13 20:58:55 +0400

    Adding  backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BotComponent } from './bot/bot.component';
@@ -15,6 +15,7 @@ import { BotComponent } from './bot/bot.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     MatSelectModule,
     MatFormFieldModule,
     AppRoutingModule,
--- src/app/bot/bot.component.ts
+++ src/app/bot/bot.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bot',
@@ -7,40 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bot.component.css']
 })
 export class BotComponent implements OnInit {
-  selected: string = '';
+  selected: string = 'How can we help?';
   greeting: string = 'Hello, how may I help you?';
-  options = ["Hotels", "Adventures", "Contact"]
+  options: string[] = []
+  default_options = []
+  api_options = []
+  isHidden: boolean = false;
+  button_text: string = 'Hide'
+  statusClass = 'unhidden';
 
-  constructor() { }
+  //option_list
+  backOption: string = ''
+  hotelOption: string = ''
+  adventureOption: string = ''
+  contactOption: string = ''
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>('http://localhost:3000/bot_options/1').subscribe(data => {
+      this.backOption = data.option1
+      this.hotelOption = data.option2 
+      this.adventureOption = data.option3
+      this.contactOption = data.option4
+      this.options = [this.hotelOption, this.adventureOption, this.contactOption]
+    })
   }
   
-  // answer_on_click(){
-  //   console.log(this.selected)
-  //   if(this.selected == "Adventures"){
-  //     this.greeting = "What kind of adventures?"
-  //   }
-  //   else if(this.selected == "Hotels"){
-  //     this.greeting = "What kind of hotels?"
-  //   }
-  //   else if(this.selected == "Contact"){
-  //     this.selected = "Our email is nobitour@gmail.com"
-  //   }
-  // }
 
-  show_change(){
-    if(this.selected == "Adventures"){
-      this.greeting = "What kind of adventures?"
-      this.options = ["London", "New York", "Monaco"]
-    }
-    else if(this.selected == "Hotels"){
-      this.greeting = "What kind of hotels?"
-      this.options = ["London", "Yerevan", "Monaco"]
-    }
-    else if(this.selected == "Contact"){
-      this.selected = "Our email is nobitour@gmail.com"
+  hide_bot(){
+    if(this.isHidden == false){
+      this.statusClass = 'hidden';
+      this.isHidden = true
+      this.button_text = "Unhide"
+    }else{
+      this.statusClass = 'unhidden';
+      this.isHidden = false
+      this.button_text = "Hide"
     }
   }
 
+  show_change(){
+      const initial_options = [this.backOption, this.hotelOption, this.adventureOption, this.contactOption]
+      if(this.selected == "Back"){
+        this.greeting = "Hello, how may I help you?'"
+        this.options = initial_options
+        this.selected = "How can we help?"
+      }
+      else if(this.selected == "Adventures"){
+        this.greeting = "What kind of adventures?"
+        this.options = ["Back", "London", "New York", "Monaco"]
+        this.selected = "List of Adventures"
+      }
+      else if(this.selected == "Hotels"){
+        this.greeting = "What kind of hotels?"
+        this.options = ["Back", "London", "Yerevan", "Monaco"]
+        this.selected = "List of Hotels"
+      }
+      else if(this.selected == "Contact"){
+        console.log("OK")
+        this.selected = "Our Email: nobitour@gmail.com Contact Phone: +123456789"
+      }  
+  }
 }
